b75adb5c8fc533f82da96ca2036fe173bf751aa9
Shubham Singh <shubham88singh@gmail.com>
2020-07-15 23:22:53 +0530

    Adding the basic layout for the search

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-async function getWeather(city = null) {
+async function getWeather(city = 'London') {
   try {
     const data = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APP_KEY}`
+      `http://api.weatherstack.com/current?access_key=${APP_KEY}&query=${city}`
     );
-    const weather = await data.json();
-    console.log(weather);
+    if (data.status === 200) {
+      const weather = await data.json();
+
+      return weather;
+    }
   } catch (err) {
     console.log(err);
   }
 }
 
-getWeather('London');
+async function showWeather(city) {
+  const response = await getWeather(city);
+
+  if (response) {
+    const city = response.location.name;
+    const country = response.location.country;
+    const status = response.current.weather_descriptions[0];
+    const icon = response.current.weather_icons[0];
+    const temperature = response.current.temperature;
+    const pressure = response.current.pressure;
+    const humidity = response.current.humidity;
+    const localtime = new Date(response.location.localtime);
+  }
+}
+
+showWeather();
